f974e073855ca9e09fd97f0e8674fa49d9091abf
Jessica <jessica@brandco.com>
2016-09-18 12:25:13 -0400

    Some serious gulpfile rearranging

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 var inject = require('gulp-inject');
 
 //To watch for sass file injections
 var wiredep = require('wiredep').stream;
 
-// var useref = require('gulp-useref');
-
 var browserSync = require('browser-sync').create();
+
+
+var imagemin = require('gulp-imagemin'); //Optimize our images
+var cache = require('gulp-cache'); //Cache optimzed images
+
 
 gulp.task('sass', function(){
 
@@ -32,32 +36,51 @@ gulp.task('sass', function(){
     //return gulp.src('src/styles/scss/*.scss') //Globbing sass files
     .pipe(inject(injectAppFiles, injectAppOptions))
     .pipe(sass())
-    .pipe(gulp.dest(''))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('src'))
     .pipe(browserSync.stream());
 });
 
-// gulp.task('useref', function(){
-//   return gulp.src('app/*.html')
-//     .pipe(useref())
-//     .pipe(gulp.dest('dist'))
+gulp.task('icons', function() { 
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest('src/fonts/')); 
+});
+
+gulp.task('images', function(){
+  return gulp.src('src/imgs/*.+(png|jpg|gif|svg)')
+  .pipe(cache(imagemin()))
+  .pipe(gulp.dest('src/imgs'))
+});
+
+// gulp.task('images', function() {
+//   return gulp.src('src/imgs/*.+(png|jpg|gif|svg)')
+// 	.pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 7, progressive: true, interlaced: true })))
+// 	.pipe(gulp.dest('src/imgs'))
+// 	.pipe(plugins.notify({ message: 'Images task complete' }));
+// });
+// gulp.task('js', function(){
+//
 // });
 
-gulp.task('html', ['sass'], function(){
-  var injectFiles = gulp.src(['dist/main.css']);
+gulp.task('inject', ['sass'], function(){
+  var sources = gulp.src(['src/main.css', 'src/main.js'], {read: false});
+  var target = gulp.src('index.html');
 
   //Remove the path to the css file, since theyre in the same folder
-  var injectOptions = {
-    addRootSlash: false,
-    ignorePath: ['src', 'dist']
-  };
+  // var injectOptions = {
+  //   addRootSlash: false,
+  //   // ignorePath: ['src', 'dist']
+  // };
 
-  return gulp.src('src/index.html')
-  .pipe(inject(injectFiles, injectOptions))
+  // return gulp.src('src/index.html')
+  // .pipe(inject(sources, injectOptions))
+  return target.pipe(inject(sources))
+  // .pipe(inject(sources))
   .pipe(gulp.dest(''))
 });
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['html'], function() {
+gulp.task('serve', ['inject'], function() {
 
     browserSync.init({
         //proxy: "index.html"
@@ -68,7 +91,8 @@ gulp.task('serve', ['html'], function() {
     });
 
     gulp.watch('src/styles/scss/*.scss', ['sass']);
-    gulp.watch('src/index.html', ['html']);
+    gulp.watch('src/js/main.js', ['js']);
+    gulp.watch('index.html', ['inject']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
